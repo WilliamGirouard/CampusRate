60566394511e0dcee125417d2206594e9635a22f
WilliamGirouard <santco123@hotmail.fr>
2026-09-13 19:45:52 -0400

    Added class-validator, class-transformer and swagger dependencies + create-places-dto.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Supprime les proprietes non definies dans le DTO.
+      forbidNonWhitelisted: true, // Lance une erreur quand il y a une propriete que l'on ne veut pas
+      transform: true, // S'assure de bien transformer avec le class-transformer
+    }),
+  );
   app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
